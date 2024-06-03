06474f29685cb4e7e20efbbb1933d48bfc3112bb
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-06-03 20:28:07 +0530

    3rd june class uploaded

---
+++ second.py
@@ -0,0 +1,49 @@
+# # hii from upflairs comment 
+# var1 = 10 
+# var2 = 20.5
+# print(var1)
+# print(type(var1)) 
+
+# print(var2)
+# print(type(var2))  
+
+
+
+
+# var3 = 12.25
+# var4 = 100.00
+
+# var5 = 112
+
+# # a-z , A-Z , 0-9 , _ 
+# int , float 
+# print('hello world')
+#data type # int , float , string
+
+# var = "upflairs 450154 @#%$^$%$"
+# var2 = 'you are all from seekar'
+
+
+# var3 = """
+# hiii we are all from sikar! and
+# varshit is very handsome student in my class.
+# nitin has very slow sytem.
+# """
+# print(var3)
+# print(type(var3))
+
+
+
+# print(var)
+# print(type(var))
+# print(var2)
+# print(type(var2))
+
+
+# company = "Upflairs"
+# print(company[::-1])
+
+# U   p   f   l  a   i   r   s
+# 0   1   2   3  4   5   6   7    #positive indexing 
+#-8  -7  -6  -5 -4  -3  -2  -1     #negative indexing
+
