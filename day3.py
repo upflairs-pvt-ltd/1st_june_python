4d091b7a6e2aa917a588117208b861c1f8a61043
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-06-04 15:57:42 +0530

    4 june

---
+++ day3.py
@@ -0,0 +1,90 @@
+# st = "UPFLAIRS pvt ltd jaipur rajasthan"
+# st = "UPFLAIRs"
+#print(len(st)) # no. of character
+# print(st.count('j'))
+# print(st.count('ja'))
+# print(st.upper())
+# print(st.lower())
+# print(st.islower())
+# print(st.isupper())
+
+
+# st = "         UPFLAIR         "
+# print(st)
+# print(st.strip())  # from left and right
+# print(st.lstrip())   # only from left
+# print(st.rstrip())   # only from right
+
+# st = "upflairs pvt ltd"
+# print(st)
+# print(st.replace("upflairs","IBM"))
+# print(st.removeprefix('up'))
+# print(st.removesuffix('ltd'))
+# print(st.replace('pvt',''))
+# print(st.replace(' ',","))
+
+# name = "rohit mohit , rahul varshit"
+# a,b,c,d = name.split()  # by default " "
+# a , b = name.split(',')
+# print(b)
+# print(name.split())
+
+# st = "upflairs"
+# print(st.center(30,"_"))
+# print(st.ljust(15,'*'))
+# print(st.rjust(15,'*'))
+# print(st.find('u'))
+
+
+
+#<<<<<<<<<<<<<< ARRAY (LIST) >>>>>>>>>>>>>>
+# student_name = ["mohit","rohit","shyam","rahul"]   # str homogenous
+# student_marks = [60,74,85,12]  # hetrogenous
+
+# print(student_name)
+# print(student_marks)
+
+# temp = [14,20.25,'upflairs']
+# print(temp)
+# print(type(temp))
+
+# student_name = ["mohit","rohit","shyam","rahul"]   
+# student_marks = [60,74,85,12]
+
+# item accessing from the list
+# print(student_name[1])
+# print(student_name[1:3])
+# a  = student_name[2] 
+# print(a) 
+# print(type(a))
+# print(a[2:])
+# a = string "shyam"   yam
+# 2 student marks + name 
+# print(student_name[2])
+# print(student_marks[2])
+
+
+# student_name = ['A','B','C','D']
+# student_name.append("E")  # insert item at the last position 
+# student_name.append("F")
+# student_name[2] = "M"
+# print(student_name)
+# student_name.pop()  # remove item from the last position
+# student_name.pop()
+# print(student_name)
+
+student_name = ['A','B','C','D']
+# item insert  "G"
+student_name.insert(2,"G")  # at any position insert
+print(student_name)
+
+
+# remove 
+st= 'upflairs'
+
+# st.isdigit()
+# st.isalnum()
+# st.endswith()
+# st.startswith()
+# remove item from list at any position 
+
