d7409f07e96c02ac63c82d1ef64879a65bd29d95
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-06-06 16:15:38 +0530

    6 june

---
+++ day5.py
@@ -0,0 +1,75 @@
+### <<<<<<<<<<<<< SET  >>>>>>>>>>>>>> 
+
+# st = {10,25.25,50,'upflairs',True}
+# st = {1,1,2,2,3,3,4,4,5,5,5,5,5,5,56,6,7,7,8,8,9,9}
+# print(st)
+# print(type(st))
+
+# st1 = {85,65,85,46,96,41,52,63,6,99}
+# st2 = {85,63,2588,963,410,41,41,41,41,41,41,41,41,41}
+# st.add(500)   # insert item in set
+# st.remove(63)  # error show 
+# st.discard(63)   # error not show
+# print(st)
+
+# print(st1.intersection(st2))  # 85,63 
+# st1.update(st2)
+# print(st1)
+
+
+
+## <<<<<<<<<<<  DICTIONARY  >>>>>>>>>>>>>>>> 
+# 10,20,30
+# key:value  <--- pairs 
+# dt = {'a':10,'b':20,'c':30}
+# print(dt['b'])
+# print(type(dt))
+
+
+# marks = {30:10,100:20,222:30}
+
+# print(marks)
+
+
+# e.g --> 5 student  
+# student = {'Name':['rahul','mohit','nitin'],
+#             'Marks':[35,45,32],
+#             'Subject':"Math"}
+
+# print(student)
+# print(type(student))
+# print("my student name :", student['Name'])
+# print("My student marks : ", student["Marks"])
+# print("Subject : ", student['Subject'])
+
+
+# dt = {'A':100,'B':200,'C':300}
+# print(dt.keys()) # only key will print 
+# print(dt.values()) # only values will print
+# print(dt['B'])
+# dt.pop('B')
+# print(dt)
+
+# dt['D'] = 400  # to insert element in dict
+# dt["B"]  = 500 
+# print(dt)
+
+student = {'Name':['rahul','mohit','nitin'],
+            'Marks':[35,45,32],
+            'Subject':"Math"}
+
+varshit = 10 
+# student['Name'].pop()
+# student['Marks'].pop()
+
+
+print(student) 
+
+
+
+
+
+
+
+
+
